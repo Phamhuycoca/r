389ca0219ebef0e60f52acbe17e04be88ea87f1f
phamhuycoca <phamkhachuy240702@gmail.com>
2025-10-22 21:11:04 +0700

    update

--- src/modules/client/main/main.tsx
+++ src/modules/client/main/main.tsx
@@ -1,37 +1,5 @@
-import { Carousel, Col, Row, Menu } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
 import BannerSection from '../BannerSection/BannerSection';
 
-const categories = [
-    "Women's Clothing",
-    'Man Fashion',
-    'Computer & Office',
-    'Jewelry & Watches',
-    'Men’s Clothing',
-    'Bags & Shoes',
-    'Toys & Kids & Baby',
-    'Automobiles',
-    'Consumer Electronics',
-    'All Accessories',
-];
-
-const banners = [
-    {
-        title1: 'New Product',
-        title2: 'Collection',
-        image: 'https://htmldemo.net/tmart/tmart/images/slider/bg/2.png',
-    },
-    {
-        title1: 'Modern Sofa',
-        title2: 'Up to 40% OFF',
-        image: 'https://htmldemo.net/tmart/tmart/images/slider/bg/2.png',
-    },
-    {
-        title1: 'Elegant Lamps',
-        title2: 'Summer Sale',
-        image: 'https://htmldemo.net/tmart/tmart/images/slider/bg/2.png',
-    },
-];
 
 const Main = () => {
     return (
